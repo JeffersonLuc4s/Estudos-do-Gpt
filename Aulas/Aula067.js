140304d96ec94909ff9095f24cc6899c332e7bae
Jefferson Lucas <103851320+JeffersonLuc4s@users.noreply.github.com>
2026-06-23 01:02:34 -0300

    Aulas 67 a 70

--- Aulas/Aula067.js
+++ Aulas/Aula067.js
@@ -1,1 +1,19 @@
-//EX1 Fácil
+//EX1 Fácil - ele guardar os itens
+//EX2 Médio - ele recupera os itens
+//EX3 Difícil - acho que o array vai ficar salvo, então toda vez que vamos atualizar a pagina o array já vai estar salvo
+
+//EX1 Fácil - nome é a chave que está sendo salva
+//EX2 Médio - jeff é o valor que está sendo salvo
+//EX3 Difícil - vai ficar salvo os dois a chave jogo e o valor PoE 2
+
+//EX1 - nome
+//EX2 - Jeff
+//EX3 - para ele conseguir achar o valor certo que estamos procurando
+
+//EX1 - ele transforma o texto no formato json
+//EX2 - ele transforma json em texto
+//EX3 - JSON.stringfy() -> local storage -> JSON.parse() -> array
+
+//EX1 - ele vira texto, pois vai adicionar aspas entre o array
+//EX2 - ele volta a ser um array pois vai perder as aspas
+//EX3 - porque o local storage só salva texto
