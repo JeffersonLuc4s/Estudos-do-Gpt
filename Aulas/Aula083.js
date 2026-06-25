252afa468652f0bd51f3cd51cf48cb4c1e0f3b7e
Jefferson Lucas <103851320+JeffersonLuc4s@users.noreply.github.com>
2026-06-25 01:38:38 -0300

    Aula 83

---
+++ Aulas/Aula083.js
@@ -0,0 +1,11 @@
+//EX1 Fácil
+const tarefas = []
+function salvarTarefas() {
+
+}
+//EX2 Médio
+const tarefas = []
+function salvarTarefas() {
+    const texto = JSON.stringify(tarefas)
+}
+//EX3 Difícil
